2cca7ea29322692d07ac46493fe90cc8a0020831
Tomáš Svatek <svatektomas117@gmail.com>
2022-03-02 21:40:25 +0100

    Initial Commit

---
+++ Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by Tomáš Svatek on 02.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
